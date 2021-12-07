88ad84b60585c0a62dce84d6ebb2e0dda185a5c3
Hatus <hatusn@gmail.com>
2021-12-07 16:11:26 -0300

    Estilizando a Tab Navigation

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
+import { Platform } from 'react-native';
+import { useTheme } from 'styled-components/native';
+import { RFValue } from 'react-native-responsive-fontsize';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { MaterialIcons } from '@expo/vector-icons';
 
 import { Dashboard } from '../screens/Dashboard';
 import { Register } from '../screens/Register';
@@ -7,11 +11,55 @@ import { Register } from '../screens/Register';
 const { Navigator, Screen } = createBottomTabNavigator();
 
 export const AppRoutes: React.FC = () => {
+  const theme = useTheme();
+
   return (
-    <Navigator>
-      <Screen name="Listagem" component={Dashboard} />
-      <Screen name="Cadastrar" component={Register} />
-      <Screen name="Resumo" component={Register} />
+    <Navigator
+      screenOptions={{
+        headerShown: false, // oculta o cabeçalho da navegação
+        tabBarActiveTintColor: theme.colors.secondary, // cor do icone/label ativos
+        tabBarInactiveTintColor: theme.colors.text, // cor do icone/label inativos
+        tabBarLabelPosition: 'beside-icon', // posição do icone em relação ao label
+        tabBarStyle: {
+          height: RFValue(88),
+          paddingVertical: Platform.OS === 'ios' ? 20 : 0,
+        },
+      }}
+    >
+      <Screen
+        name="Listagem"
+        component={Dashboard}
+        options={{
+          // size - tamanho do icone; color - cor do icone
+          tabBarIcon: ({ size, color }) => (
+            <MaterialIcons
+              name="format-list-bulleted"
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+
+      <Screen
+        name="Cadastrar"
+        component={Register}
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <MaterialIcons name="attach-money" size={size} color={color} />
+          ),
+        }}
+      />
+
+      <Screen
+        name="Resumo"
+        component={Register}
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <MaterialIcons name="pie-chart" size={size} color={color} />
+          ),
+        }}
+      />
     </Navigator>
   );
 };
