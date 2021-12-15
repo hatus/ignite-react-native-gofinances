c8e49dfaac2f1805b3686f5bde37237e2d17484c
Hatus <hatusn@gmail.com>
2021-12-15 14:58:34 -0300

    Criando a interface de gráfico

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -7,6 +7,7 @@ import { MaterialIcons } from '@expo/vector-icons';
 
 import { Dashboard } from '../screens/Dashboard';
 import { Register } from '../screens/Register';
+import { Resume } from '../screens/Resume';
 import { AppRoutesParamList } from '../utils/routes';
 
 const { Navigator, Screen } = createBottomTabNavigator<AppRoutesParamList>();
@@ -54,7 +55,7 @@ export const AppRoutes: React.FC = () => {
 
       <Screen
         name="Resumo"
-        component={Register}
+        component={Resume}
         options={{
           tabBarIcon: ({ size, color }) => (
             <MaterialIcons name="pie-chart" size={size} color={color} />
